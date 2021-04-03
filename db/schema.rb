5c555d1108bc1a8709eebef485e77649056620f3
Vladimir <konceropyatovladimir@gmail.com>
2021-04-03 22:16:18 +0300

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_02_214901) do
+ActiveRecord::Schema.define(version: 2021_04_03_190128) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 2021_04_02_214901) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "author"
     t.text "body"
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
