c0ae095211f13118d10e045399eab9f891a885f3
Vladimir <konceropyatovladimir@gmail.com>
2021-04-10 12:23:27 +0300

    add length validation on article text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
     validates :title, presence: true
-    validates :text, presence: true
+    validates :text, presence: true, length: { minimum: 20 }
+    
     has_many :comments 
+
 end
