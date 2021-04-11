467bb15e80660aa21d5b7726dbcb69bd05986fc4
Vladimir <konceropyatovladimir@gmail.com>
2021-04-12 00:43:55 +0300

    add user depends in article and comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :text, presence: true, length: { minimum: 20 }
     
+    belongs_to :user
     has_many :comments, dependent: :destroy 
 end
