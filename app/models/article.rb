41bb7dedae84a92e7b0c8935a1794f8ef27f91c4
Vladimir <konceropyatovladimir@gmail.com>
2021-04-11 19:17:31 +0300

    add dependion on comment in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,5 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :text, presence: true, length: { minimum: 20 }
     
-    has_many :comments 
-
+    has_many :comments, dependent: :destroy 
 end
