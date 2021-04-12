ad4639799a5ac7ed0ad78c89b34ad75c2d48af4e
Vladimir <konceropyatovladimir@gmail.com>
2021-04-12 14:47:46 +0300

    remove change last 2 commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,5 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :text, presence: true, length: { minimum: 20 }
     
-    belongs_to :user
     has_many :comments, dependent: :destroy 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_215139) do
+ActiveRecord::Schema.define(version: 2021_04_02_213244) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
+    t.string "author"
     t.text "body"
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -49,10 +46,7 @@ ActiveRecord::Schema.define(version: 2021_04_11_215139) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
-  add_foreign_key "comments", "users"
 end
